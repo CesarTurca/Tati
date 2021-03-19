128cba732326ef7dcfe6230eda0d9eb5923f0a4e
CesarTurca <79059323+CesarTurca@users.noreply.github.com>
2021-03-18 23:53:16 -0500

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2021, // required
 	month: 3, // required
-	day: 18, // required
+	day: 17, // required
 	hours: 19, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
